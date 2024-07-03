5a1ae47e41adc0b57b698189be62022b447cd970
ledo9124 <leminhdo904@gmail.com>
2024-07-03 17:08:33 +0700

    Connect to MonggoDB

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-import express from "express";
+import express from 'express';
+import 'dotenv/config';
+import connectToMongoDB from './db/connectToMongoDB.js';
+
+const app = express()
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    connectToMongoDB();
+  console.log(`Example app listening on port ${port}`)
+})
